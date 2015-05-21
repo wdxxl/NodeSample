1b29d6208a18f83c2b93804d721474311db9271e
king.wang <wdxxlanswer@gmail.com>
2015-05-21 18:19:04 +0800

    add ejs page details.

--- 04_Mongoose/routes/index.js
+++ 04_Mongoose/routes/index.js
@@ -19,4 +19,21 @@ router.get('/logout',function(req,res){
     res.render('logout',{title:'logout'});
 });
 
+/*hompage*/
+router.post('/homepage', function(req, res) {
+    var query_doc = {userid: req.body.userid, password: req.body.password};
+    (function(){
+        user.count(query_doc, function(err, doc){
+            if(doc == 1){
+                console.log(query_doc.userid + ": login success in " + new Date());
+                res.render('homepage', { title: 'homepage' });
+            }else{
+                console.log(query_doc.userid + ": login failed in " + new Date());
+                res.redirect('/');
+            }
+        });
+    })(query_doc);
+});
+
+module.exports = router;
 

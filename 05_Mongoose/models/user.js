6ebc02666cc00864b9e2224e15d39c383f45579b
king.wang <wdxxlanswer@gmail.com>
2015-05-21 21:52:40 +0800

    add 05_mongoose sample.

---
+++ 05_Mongoose/models/user.js
@@ -0,0 +1,11 @@
+/**
+ * Created by wangkexue on 2015/5/21.
+ */
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var userSchama = new Schema({
+    userid:String,
+    password:String
+});
+
+exports.user = mongoose.model('users',userSchama);

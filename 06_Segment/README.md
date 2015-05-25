95b7fe7e7faf549034e854f9d10b05abde5d2c93
king.wang <wdxxlanswer@gmail.com>
2015-05-25 10:30:02 +0800

    segment - Chinese word segmentation 中文分词模块 例子。

---
+++ 06_Segment/README.md
@@ -0,0 +1,3 @@
+#Segment Chinese word segmentation 中文分词模块
+
+https://www.npmjs.com/package/segment
---
+++ 06_Segment/routes/segment.js
@@ -0,0 +1,16 @@
+﻿var express = require('express');
+var Segment = require('segment');
+var router = express.Router();
+
+var segment = new Segment();
+// 使用默认的识别模块及字典，载入字典文件需要1秒，仅初始化时执行一次即可
+segment.useDefault();
+// 开始分词
+//console.log(segment.doSegment('这是一个基于Node.js的中文分词模块。'));
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send(segment.doSegment('这是一个基于Node.js的中文分词模块。'));
+});
+
+module.exports = router;
